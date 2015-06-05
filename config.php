faee4c88ca3afd60e8e75ff733fed34ee36894c2
Juan Antonio Valera <j.v.metall@gmail.com>
2015-06-05 10:44:23 +0200

    Añadida plantilla administrador

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
-    ORM::configure('mysql:host=(NOMBREHOST Ej:localhost);dbname=NOMBREDELABASEDEDATOS; charset=utf8');
+    ORM::configure('mysql:host=localhost;dbname=NOMBREDELABASEDEDATOS; charset=utf8');
     ORM::configure('username','USUARIODELABASEDEDATOS');
     ORM::configure('password','CONTRASEÑADELABASEDEDATOS');
